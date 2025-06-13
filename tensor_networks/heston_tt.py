52112557a6055c834aa968094d773b25d0a90c88
ben256 <ben@naylorweb.co.uk>
2025-06-13 12:56:02 +0100

    added torch reqs

--- tensor_networks/heston_tt.py
+++ tensor_networks/heston_tt.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--tt_tolerance', type=float, default=1e-4)
     parser.add_argument('--tt_rmax', type=int, default=50)
-    parser.add_argument('--tt_max_iter', type=int, default=2)
+    parser.add_argument('--tt_max_iter', type=int, default=50)
 
     args = parser.parse_args()
     main(tt_tolerance=args.tt_tolerance, tt_rmax=args.tt_rmax, tt_max_iter=args.tt_max_iter)
